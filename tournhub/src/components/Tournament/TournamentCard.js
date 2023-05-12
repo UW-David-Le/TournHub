b11bb8571831e39addb21ee68824a0a87c887cd0
andrewkfso <so.kf.andrew@gmail.com>
2023-05-12 14:57:47 -0400

    tournament card

--- tournhub/src/components/Tournament/TournamentCard.js
+++ tournhub/src/components/Tournament/TournamentCard.js
@@ -1,32 +1,66 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import image from "../../assets/tourn1.jpg";
-import { FaBeer } from 'react-icons/fa'; // <FaBeer />
+// import { FaBeer } from 'react-icons/fa'; // <FaBeer />
 import { FaRegCalendar } from 'react-icons/fa';
+import { AiOutlineClockCircle } from 'react-icons/ai'
+import eventData from './eventData';
 
 
 function TournamentCard() {
-    return (
+
+    const eData = eventData.events;
+
+    const listEvents = eData.map(event =>
+        
         <Card className="border border-dark" style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={image} />
+            {console.log(event.thumbnail)}
+            {console.log(image)}
+            <Card.Img variant="top" src={event.thumbnail} />
             <Card.Body>
                 <Card.Title>Card Title</Card.Title>
                 <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
+                    {event.description}
                 </Card.Text>
                 <hr />
                 <Card.Text className=''>
-                    <FaRegCalendar />
-                    more text
+                    <FaRegCalendar /> {event.date}  <AiOutlineClockCircle /> {event.startTime}
                 </Card.Text>
                 <hr />
-                <Button className="btn" variant="primary">Go somewhere</Button>
+                <Button className="btn" variant="primary">Sign Up!</Button>
             </Card.Body>
             <Card.Footer>
-                Organizer's Name
+                {event.organizer}
             </Card.Footer>
         </Card>
+    );
+
+    return (
+        <div>
+            {listEvents}
+        </div>
+        
+        // {listEvents}
+        // <Card className="border border-dark" style={{ width: '18rem' }}>
+        //     <Card.Img variant="top" src={image} />
+        //     <Card.Body>
+        //         <Card.Title>Card Title</Card.Title>
+        //         <Card.Text>
+        //             Some quick example text to build on the card title and make up the
+        //             bulk of the card's content.
+        //         </Card.Text>
+        //         <hr />
+        //         <Card.Text className=''>
+        //             <FaRegCalendar />
+        //             more text
+        //         </Card.Text>
+        //         <hr />
+        //         <Button className="btn" variant="primary">Go somewhere</Button>
+        //     </Card.Body>
+        //     <Card.Footer>
+        //         Organizer's Name
+        //     </Card.Footer>
+        // </Card>
     );
 }
 
---
+++ tournhub/src/components/Tournament/eventData.js
@@ -0,0 +1,39 @@
+// Dummy data for event (Tournaments/Leagues)
+// Event Title, Date, Location, Description, Thumbnail, Capacity, Organizer
+
+const eventData = {
+    events: [
+        {
+            title: "Co-ed 2v2 Beach 4s Int/Comp Mixed",
+            date: "May 11, 2023",
+            location: "Concord",
+            description: "Early Bird - $240 per team \n Late Bird - $300 per team \n 1st Place Prize $360 and YYZPrints Beach Swag",
+            thumbnail: "../../tourn1.jpg",
+            capacity: "13/24",
+            organizer: "YYZAthletics",
+            startTime: "9:00AM EST"
+        },
+        {
+            title: "Co-ed 4:2 All Nighter Volleyball Tournament",
+            date: "May 16, 2023",
+            location: "Mississauga",
+            description: "Early Bird - $240 per team \n Late Bird - $300 per team \n 1st Place Prize $360 and YYZAth Swag",
+            thumbnail: "../../tourn2.jpg",
+            capacity: "19/24",
+            organizer: "YYZAthletics",
+            startTime: "10:30PM EST"
+        },
+        {
+            title: "Mens 6's Volleyball Tournament",
+            date: "May 20, 2023",
+            location: "Malton",
+            description: "Early Bird - $280 per team \n Not So Early Bird - $300 per team \n Prizes: $400/$200/$100",
+            thumbnail: "../../tourn3.jpg",
+            capacity: "19/24",
+            organizer: "MJenVb",
+            startTime: "1:00PM EST"
+        }
+    ]
+}
+
+export default eventData;
